81dbae531a50eb1b9db765875efc6bfe9e0b6120
dduddu7 <lsj276324@gmail.com>
2024-01-14 20:05:42 +0900

    수량 조절 기능 수정중114

--- src/components/commons/layout/basket/Basket.container.tsx
+++ src/components/commons/layout/basket/Basket.container.tsx
@@ -67,27 +67,92 @@
 //   );
 // };
 
+
+// import React, { useEffect, useState } from "react";
+// import { AllPayment, Order, Payment, PaymentBtn, PaymentInfo, OrderList, Title, Wrapper } from "./Basket.style";
+// import { useBasket } from "./basketUtils";
+// import { Quantity } from "../../quantity/Quantity.container";
+
+// export const Basket = () => {
+//   const { basketItem} = useBasket();
+//   const [totalPayment, setTotalPayment] = useState(0);
+
+//   const handlePayment = (newQuantity: number) => {
+//     // 주문한 메뉴들의 총 가격을 계산
+//     const newTotalPayment = basketItem.reduce(
+//       (total, menuItem) => total + menuItem.price * newQuantity, 0
+//     );
+//     setTotalPayment(newTotalPayment);
+//     console.log(newTotalPayment);
+//   };
+  
+//   useEffect(() => {
+//     // basketItem이 변경될 때마다 handlePayment 함수 호출
+//     handlePayment(1); // 1은 기본 수량, 원하는 값으로 변경하세요.
+//   }, [basketItem]);
+
+
+//   return (
+//     <Wrapper>
+//       <Order>
+//         <Title>
+//           order
+//           <span>
+//             총 <em>{basketItem.length}</em>개
+//           </span>
+//         </Title>
+
+//         <OrderList>
+//           {/* 주문한 메뉴들을 렌더링 */}
+//           {basketItem.map((menuItem, index) => (
+//             <div key={index}>
+//               메뉴 이름 : {menuItem.name}
+//               <br />
+//               가격 : {menuItem.price}원
+//               <Quantity onChange={(newQuantity) => handlePayment(newQuantity)} />
+//             </div>
+//           ))}
+//         </OrderList>
+//       </Order>
+
+//       <Payment>
+//         <PaymentInfo>{/* 결제 정보 */}</PaymentInfo>
+//         <AllPayment>
+//           <span>총 결제금액</span>
+//           <span>
+//             <em>{totalPayment}</em>원
+//           </span>
+//         </AllPayment>
+
+//         <PaymentBtn>결제하기</PaymentBtn>
+//       </Payment>
+//     </Wrapper>
+//   );
+// };
+
 import React, { useEffect, useState } from "react";
 import { AllPayment, Order, Payment, PaymentBtn, PaymentInfo, OrderList, Title, Wrapper } from "./Basket.style";
 import { useBasket } from "./basketUtils";
-import { Quantity } from "../../quantity/Quantity.container";
+import { Quantity } from "../../quantity/Quantity.container"; // 경로는 실제 폴더 구조에 맞게 변경
 
 export const Basket = () => {
   const { basketItem } = useBasket();
   const [totalPayment, setTotalPayment] = useState(0);
 
-  const handlePayment = (newQuantity: number) => {
+  const handlePayment = (itemName: string, newQuantity: number) => {
     // 주문한 메뉴들의 총 가격을 계산
-    const newTotalPayment = basketItem.reduce(
-      (total, menuItem) => total + menuItem.price * newQuantity, 0
-    );
+    const newTotalPayment = basketItem.reduce((total, menuItem) => {
+      if (menuItem.name === itemName) {
+        return total + menuItem.price * newQuantity;
+      }
+      return total + menuItem.price * (menuItem.quantity || 1); // 다른 메뉴들은 이전 수량 유지
+    }, 0);
     setTotalPayment(newTotalPayment);
-    console.log(newTotalPayment);
   };
 
   useEffect(() => {
     // basketItem이 변경될 때마다 handlePayment 함수 호출
-    handlePayment(1); // 1은 기본 수량, 원하는 값으로 변경하세요.
+    handlePayment("", 1); // 1은 기본 수량, 원하는 값으로 변경하세요.
   }, [basketItem]);
 
   return (
@@ -104,10 +169,14 @@ export const Basket = () => {
           {/* 주문한 메뉴들을 렌더링 */}
           {basketItem.map((menuItem, index) => (
             <div key={index}>
-              메뉴 이름 : {menuItem.name}
+              메뉴 이름: {menuItem.name}
               <br />
-              가격 : {menuItem.price}원
-              <Quantity onChange={(newQuantity) => handlePayment(newQuantity)} />
+              가격: {menuItem.price}원
+              {/* Quantity 컴포넌트를 가져와서 사용 */}
+              <Quantity
+                onChange={(newQuantity) => handlePayment(menuItem.name, newQuantity)}
+                defaultQuantity={menuItem.quantity || 1}
+              />
             </div>
           ))}
         </OrderList>
@@ -122,8 +191,10 @@ export const Basket = () => {
           </span>
         </AllPayment>
 
-        <PaymentBtn onClick={() => handlePayment(1)}>결제하기</PaymentBtn>
+        <PaymentBtn>결제하기</PaymentBtn>
       </Payment>
     </Wrapper>
   );
 };
+
+
